2d026879538f81389ffb042efb4307fca77f3e46
pierre crochet <pierrecrochet03@gmail.com>
2021-01-27 13:50:23 +0000

    API is functional, no more useless routes or html file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
-  acts_as_token_authentication_handler_for User
+  before_action :authenticate_user!
+  # acts_as_token_authentication_handler_for User
 end
