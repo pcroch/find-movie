0b0262108089661be1f3945ec0f43c6cd310ebd2
pierre crochet <pierrecrochet03@gmail.com>
2021-02-01 12:29:20 +0000

    home page

--- app/controllers/api/v1/finders_controller.rb
+++ app/controllers/api/v1/finders_controller.rb
@@ -1,3 +1,4 @@
+
 class Api::V1::FindersController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [:index, :show]
   before_action :set_finder, only: [:show, :update, :destroy]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users , skip: :all
-  # root to: 'pages#home'
-root to: 'api/v1/finders#index'
+  root to: 'pages#home'
+# root to: 'api/v1/finders#index'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
