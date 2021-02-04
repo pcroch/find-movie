1a130d5445df21abc70d14da08fd613df2f225a5
pierre crochet <pierrecrochet03@gmail.com>
2021-02-04 14:48:30 +0000

    basic integration testing done
    Basucally, the error handeling for the finder model is done but also for the finder controller

--- spec/models/finder_spec.rb
+++ spec/models/finder_spec.rb
@@ -1,21 +1,220 @@
-# frozen_string_literal: true
-# require 'rails_helper'
+RSpec.describe 'Error handeling testing when creating an event:', type: :request do
 
-# RSpec.describe Finder, type: :model do
-#   # pending "add some examples to (or delete) #{__FILE__}"
+ describe 'Missing the release params' do
+    let!(:user) { User.create(email: 'test@test.test', password: 'testest', user_name: 'TestUser', authentication_token: 'KdapjiY6vz-sBkKmNabc', id: 1) }
+    let!(:pref) { Preference.create(user_id: 1, name: 'Test', content: %w[Action Comedy Horror]) }
 
-#     context 'validation test' do
-#     it 'ensures first name presence' do
-#       event = Finder.create(release: "2008", duration: "200", rating: ["5", "10"], user_id: "1").save
-#       expect(event).to eq("aa")
-#     end
-# end
+    before do
+      sample_body = { "finder": {
 
-#     context 'validation test' do
-#     it 'ensures first name presence' do
-#       event = Finder.new(release: "2008", duration: "200", rating: ["5", "10"], user_id: "1")
-#     assert(event.save, "Saved the article without a title")
-#     end
-# end
+        "duration": 200,
+        "attendees": ['Test'],
+        "rating": [0, 10]
+      } }
 
-# end
+      post 'http://localhost:3000/api/v1/finders', params: sample_body, headers: { 'X-User-Email': user.email, 'X-User-Token': user.authentication_token }
+    end
+    error= {"errors"=>["Release : The integer must be a 4 digits", "Release can't be blank"]}
+
+    it 'returns an error message' do
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq(error)
+
+    end
+
+    it 'returns status code created 422' do
+      expect(response).to have_http_status(422)
+    end
+  end
+
+  describe 'A Negatif Release params' do
+    let!(:user) { User.create(email: 'test@test.test', password: 'testest', user_name: 'TestUser', authentication_token: 'KdapjiY6vz-sBkKmNabc', id: 1) }
+    let!(:pref) { Preference.create(user_id: 1, name: 'Test', content: %w[Action Comedy Horror]) }
+
+    before do
+      sample_body = { "finder": {
+        "release": -2,
+        "duration": 200,
+        "attendees": ['Test'],
+        "rating": [0, 10]
+      } }
+
+      post 'http://localhost:3000/api/v1/finders', params: sample_body, headers: { 'X-User-Email': user.email, 'X-User-Token': user.authentication_token }
+    end
+    error= {:errors=>["Duration must be a positive integer"]}
+
+
+    it 'returns an error message' do
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq(error)
+
+    end
+
+    it 'returns status code created 422' do
+      expect(response).to have_http_status(422)
+    end
+  end
+
+  describe 'Release params length bigger than 4 digits' do
+    let!(:user) { User.create(email: 'test@test.test', password: 'testest', user_name: 'TestUser', authentication_token: 'KdapjiY6vz-sBkKmNabc', id: 1) }
+    let!(:pref) { Preference.create(user_id: 1, name: 'Test', content: %w[Action Comedy Horror]) }
+
+    before do
+      sample_body = { "finder": {
+        "release": 10_000,
+        "duration": 200,
+        "attendees": ['Test'],
+        "rating": [0, 10]
+      } }
+
+      post 'http://localhost:3000/api/v1/finders', params: sample_body, headers: { 'X-User-Email': user.email, 'X-User-Token': user.authentication_token }
+    end
+    error= {"errors":["Release : The integer must be a 4 digits"]}
+
+    it 'returns an error message' do
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq(error)
+
+    end
+
+    it 'returns status code created 422' do
+      expect(response).to have_http_status(422)
+    end
+  end
+
+   describe 'Missing the duration params' do
+    let!(:user) { User.create(email: 'test@test.test', password: 'testest', user_name: 'TestUser', authentication_token: 'KdapjiY6vz-sBkKmNabc', id: 1) }
+    let!(:pref) { Preference.create(user_id: 1, name: 'Test', content: %w[Action Comedy Horror]) }
+
+    before do
+      sample_body = { "finder": {
+
+        "release": 1900,
+        "attendees": ['Test'],
+        "rating": [0, 10]
+      } }
+
+      post 'http://localhost:3000/api/v1/finders', params: sample_body, headers: { 'X-User-Email': user.email, 'X-User-Token': user.authentication_token }
+    end
+    error= {"errors"=>["Duration can't be blank", "Duration must be a positive integer"]}
+
+    it 'returns an error message' do
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq(error)
+
+    end
+
+    it 'returns status code created 422' do
+      expect(response).to have_http_status(422)
+    end
+  end
+
+  describe 'Negatif Duration params size' do
+    let!(:user) { User.create(email: 'test@test.test', password: 'testest', user_name: 'TestUser', authentication_token: 'KdapjiY6vz-sBkKmNabc', id: 1) }
+    let!(:pref) { Preference.create(user_id: 1, name: 'Test', content: %w[Action Comedy Horror]) }
+
+    before do
+      sample_body = { "finder": {
+        "release": 1900,
+        "duration": -2,
+        "attendees": ['Test'],
+        "rating": [0, 10]
+      } }
+
+      post 'http://localhost:3000/api/v1/finders', params: sample_body, headers: { 'X-User-Email': user.email, 'X-User-Token': user.authentication_token }
+    end
+    error= {"errors"=>["Duration must be a positive integer"]}
+
+    it 'returns an error message' do
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq(error)
+
+    end
+
+    it 'returns status code created 422' do
+      expect(response).to have_http_status(422)
+    end
+  end
+
+     describe 'Missing the rating params' do
+    let!(:user) { User.create(email: 'test@test.test', password: 'testest', user_name: 'TestUser', authentication_token: 'KdapjiY6vz-sBkKmNabc', id: 1) }
+    let!(:pref) { Preference.create(user_id: 1, name: 'Test', content: %w[Action Comedy Horror]) }
+
+    before do
+      sample_body = { "finder": {
+        "release": 1900,
+        "duration": 190,
+
+        "attendees": ["Pierre"]
+
+      }}
+
+      post 'http://localhost:3000/api/v1/finders', params: sample_body, headers: { 'X-User-Email': user.email, 'X-User-Token': user.authentication_token }
+    end
+    error= {"errors"=>["Rating can't be blank", "Rating must be an array of two elements"]}
+
+    it 'returns an error message' do
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq(error)
+
+    end
+
+    it 'returns status code created 422' do
+      expect(response).to have_http_status(422)
+    end
+  end
+
+       describe 'Missing the attendees params' do
+    let!(:user) { User.create(email: 'test@test.test', password: 'testest', user_name: 'TestUser', authentication_token: 'KdapjiY6vz-sBkKmNabc', id: 1) }
+    let!(:pref) { Preference.create(user_id: 1, name: 'Test', content: %w[Action Comedy Horror]) }
+
+    before do
+      sample_body = { "finder": {
+        "release": 1900,
+        "duration": 190,
+        "rating": [1, 10]
+
+      }}
+
+      post 'http://localhost:3000/api/v1/finders', params: sample_body, headers: { 'X-User-Email': user.email, 'X-User-Token': user.authentication_token }
+    end
+    error= {"errors"=>["attendees can't be blank", "attendees must be an array of at least one elements"]}
+
+    it 'returns an error message' do
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq(error)
+
+    end
+
+    it 'returns status code created 422' do
+      expect(response).to have_http_status(422)
+    end
+  end
+
+         describe 'Empty response' do
+    let!(:user) { User.create(email: 'test@test.test', password: 'testest', user_name: 'TestUser', authentication_token: 'KdapjiY6vz-sBkKmNabc', id: 1) }
+    let!(:pref) { Preference.create(user_id: 1, name: 'Test', content: %w[Action Comedy Horror]) }
+
+    before do
+      sample_body = { "finder": {
+        "release": 1900,
+        "duration": 190,
+        "rating": [1, 10]
+
+      }}
+
+      post 'http://localhost:3000/api/v1/finders', params: sample_body, headers: { 'X-User-Email': user.email, 'X-User-Token': user.authentication_token }
+    end
+    error= {"errors"=>["No movie found"]}
+
+    it 'returns an warning message' do
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq(error)
+
+    end
+
+    it 'returns status code created 404' do
+      expect(response).to have_http_status(404)
+    end
+  end
+end
